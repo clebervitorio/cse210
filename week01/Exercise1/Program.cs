2201747ff77544685b690c9f8ded2049b947e423
clebervitorio <clebervitorio@gmail.com>
2025-03-08 22:52:59 +0000

    Atualizing all exercises of the week01

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    //  Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+    //  Show on the screen phrase What is your first name?
+    //  and accepting on screen the first name of the person
+        Console.Write("What is your first name? ");
+        string first = Console.ReadLine();
+
+    //  Show on the screen phrase What is your last name?
+    //  and accepting on screen the first name of the person
+        Console.Write("What is your last name? ");
+        string last = Console.ReadLine();
+
+    //  Show on the screen phrase Your name is with last and first name
+        Console.WriteLine($"Your name is {last}, {first} {last}.");    
     }
 }
